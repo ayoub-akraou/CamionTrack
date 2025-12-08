cc1b7bda992b980a24b3078f3a7324b38c75c068
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-12-08 12:05:59 +0100

    feat(services): create AuthService.register()

---
+++ api/src/services/auth.service.js
@@ -0,0 +1,19 @@
+import UserRepository from "../services/auth.repository.js";
+
+export default class AuthService {
+  static async register(data) {
+    const { email } = data;
+    const isAlreadyExist = await UserRepository.findByEmail(email);
+
+    if (isAlreadyExist) {
+      const error = new Error(
+        "email already exist! use another email or login!"
+      );
+      error.statusCode = 400;
+      throw error;
+    }
+
+    const user = await UserRepository.createUser(data);
+    return user;
+  }
+}
