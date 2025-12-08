80b6a37e7dde3134c553b96a893397e9d38a7e50
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-12-08 11:42:12 +0100

    feat(models): create user model

---
+++ api/src/models/user.model.js
@@ -0,0 +1,31 @@
+import { Schema, model } from "mongoose";
+
+const userShema = new Schema(
+  {
+    username: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ["driver", "admin"], default: "driver" },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      transform: (_, ret) => {
+        ret.id = ret._id;
+        delete ret._id;
+        return ret;
+      },
+      //   toObject: {
+      //     transform: (_, ret) => {
+      //       ret.id = ret._id;
+      //       delete ret._id;
+      //       return ret;
+      //     },
+      //   },
+    },
+  }
+);
+
+const UserModel = model("User", userShema);
+
+export default UserModel;
