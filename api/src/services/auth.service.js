1edb445179352219b5cf9313229676385f7480ea
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-12-08 15:17:41 +0100

    feat: create AuthService.login()

--- api/src/services/auth.service.js
+++ api/src/services/auth.service.js
@@ -16,4 +16,29 @@ export default class AuthService {
     const user = await UserRepository.createUser(data);
     return user;
   }
+
+  static async login(data) {
+    const { email, password } = data;
+    const user = await UserRepository.findByEmail(email, true);
+
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      const error = new Error("Invalid credentials");
+      error.statusCode = 401; // Unauthorized
+      throw error;
+    }
+
+    const payload = {
+      id: user.id,
+      email: user.email,
+      role: user.role,
+    };
+
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      /* expiresIn: "1d" */
+    });
+
+    delete user.password;
+
+    return { user, token };
+  }
 }
