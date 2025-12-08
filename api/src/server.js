605080df0da810d7d1b35a3266daf4897de15351
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-12-08 14:37:09 +0100

    feat'server.js): connect to mongodb database

--- api/src/server.js
+++ api/src/server.js
@@ -1,10 +1,16 @@
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
 dotenv.config();
 
 const app = express();
+
+// connect to db
+connectDB();
+
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
