bc4e143fd9f62f40b8ab46eaf1530c2e9e32c577
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-12-08 14:38:05 +0100

    feat(models): add unique restriction on email

--- api/src/models/user.model.js
+++ api/src/models/user.model.js
@@ -3,7 +3,7 @@ import { Schema, model } from "mongoose";
 const userShema = new Schema(
   {
     username: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: String, enum: ["driver", "admin"], default: "driver" },
   },
