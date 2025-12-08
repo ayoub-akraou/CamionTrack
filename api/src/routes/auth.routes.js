4bdac0d1376ceb4ae7757cb0fd9a93285b3de3b9
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-12-08 11:50:19 +0100

    feat(routes): add POST /register

---
+++ api/src/routes/auth.routes.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import AuthController from "../controllers/auth.controller.js";
+const router = Router();
+
+router.post("/register", AuthController.register);
+
+export default router;
