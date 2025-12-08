046f8c66a7a5f4eb4ea996fcc36a2631aa7d3646
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-12-08 15:12:59 +0100

    feat(routes): add POST /login

--- api/src/routes/auth.routes.js
+++ api/src/routes/auth.routes.js
@@ -3,5 +3,5 @@ import AuthController from "../controllers/auth.controller.js";
 const router = Router();
 
 router.post("/register", AuthController.register);
-
+router.post("/login", AuthController.login);
 export default router;
