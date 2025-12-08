5998465acee0f67ee694e20450919b1901e9a7bc
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-12-08 14:40:09 +0100

    feat: mount auth routes in the server

--- api/src/server.js
+++ api/src/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import helmet from "helmet";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
 
@@ -14,6 +15,9 @@ connectDB();
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
+
+// routes
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
