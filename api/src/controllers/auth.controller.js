5663a3e369f388817689d0ec80a28d05eb66d71b
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-12-08 11:59:44 +0100

    feat(auth): create AuthController.register()

---
+++ api/src/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+export default class AuthController {
+  static async register(req, res) {
+    try {
+      const user = await AuthService.register(req.body);
+      res.status(201).json({ success: true, data: user });
+    } catch (error) {
+      res
+        .status(error.statusCode || 500)
+        .json({ success: false, message: error.message });
+    }
+  }
+}
