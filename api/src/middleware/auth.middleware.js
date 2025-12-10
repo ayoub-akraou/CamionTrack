fdf7bcff977657afcf756b1f5f090a0da7d32d38
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-12-10 16:06:35 +0100

    feat(middlewares): create authentication middleware

---
+++ api/src/middleware/auth.middleware.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+export function authenticate(req, res, next) {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token)
+    return res.status(401).json({ success: false, message: "connectez-vous!" });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(401).json({ success: false, message: "Token invalide" });
+  }
+}
