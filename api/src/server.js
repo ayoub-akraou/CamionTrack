135cce8ff2eae6d3d284e5a0439463702f2a0ba0
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-12-10 16:39:47 +0100

    feat: apply authentication middleware

--- api/src/server.js
+++ api/src/server.js
@@ -7,6 +7,7 @@ import authRoutes from "./routes/auth.routes.js";
 import vehiclesRoutes from "./routes/vehicles.routes.js";
 import tripsRoutes from "./routes/trips.routes.js";
 import configRoutes from "./routes/configuration.routes.js";
+import { authenticate } from "./middleware/auth.middleware.js";
 
 dotenv.config();
 
@@ -21,6 +22,7 @@ app.use(helmet());
 
 // routes
 app.use("/api/auth", authRoutes);
+app.use(authenticate);
 app.use("/api/vehicles", vehiclesRoutes);
 app.use("/api/trips", tripsRoutes);
 app.use("/api/config", configRoutes);
