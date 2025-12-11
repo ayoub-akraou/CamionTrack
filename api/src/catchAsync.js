b4e282eccdc9a80f46e6798d74911f9ef2397f2f
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-12-11 09:56:04 +0100

    feat(errors): add catchAsync error handler

---
+++ api/src/catchAsync.js
@@ -0,0 +1,9 @@
+export default function catchAsync(fn) {
+  return async function (req, res, next) {
+    try {
+      await fn(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
+}
