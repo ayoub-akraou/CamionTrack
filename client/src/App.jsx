6b17d7c22b4d406641e0c1c23afeb27a3f09726b
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-12-14 11:56:57 +0100

    feat: add register route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import Login from './pages/Login';
-
+import Register from './pages/Register';
 
 function App() {
   return (
@@ -9,6 +9,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </Router>
     </AuthProvider>
